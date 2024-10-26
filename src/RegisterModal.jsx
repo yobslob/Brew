e2a533ed23275b9d7f107ab3c604f69f24c2bc1c
Yogesh <yogeshxiix@gmail.com>
2024-10-27 02:02:52 +0530

    Update RegisterModal.jsx

--- src/RegisterModal.jsx
+++ src/RegisterModal.jsx
@@ -21,7 +21,7 @@ function RegisterModal({ closeModal }) {
         };
 
         // Get the backend URL from environment variables
-        const backendUrl = process.env.REACT_APP_FRONTEND_URL;
+        const backendUrl = 'https://brew-ows3.onrender.com';
         console.log(backendUrl);
         try {
             // Send a POST request to the backend
